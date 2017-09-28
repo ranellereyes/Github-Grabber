796c47fa2df70f307b411f73a0e6223b0a786f91
Ranelle <ranelle.reyes@gmail.com>
2017-09-28 07:19:32 -0700

    Add github server

--- github_grabber.js
+++ github_grabber.js
@@ -1,4 +1,21 @@
 const fs = require('fs');
-const querystring = require('querystring');
+const qs = require('querystring');
 const http = require('http');
 const https = require('https');
+
+const githubServer = http.createServer((req, res) => {
+  if (req.method === "POST") {
+    let collectedData = "";
+
+    req.on("data", datum => {
+      collectedData += datum;
+    })
+
+    req.on("end", () => {
+      const username = qs.parse(collectedData).username
+      res.end(username)
+    })
+  }
+})
+
+githubServer.listen(8080, () => { console.log(`I'm listening on port 8080!`);})
